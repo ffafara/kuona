8c27ef7f2ad2b0dff35d4ad441d74366c9f21543
Graham Brooks <graham@grahambrooks.com>
2014-09-28 17:45:37 -0400

    Removed unused fields.

--- src/main/java/kuona/subversion/Hunk.java
+++ src/main/java/kuona/subversion/Hunk.java
@@ -1,15 +1,11 @@
 package kuona.subversion;
 
 public class Hunk {
-    private int originalStart;
     private int originalLength;
-    private int newStart;
     private int newLength;
 
     public Hunk(int originalStart, int originalLength, int newStart, int newLength) {
-        this.originalStart = originalStart;
         this.originalLength = originalLength;
-        this.newStart = newStart;
         this.newLength = newLength;
     }
 
