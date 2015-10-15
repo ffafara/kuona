adea54d0300ac3ec488e636d9f5e6094575aeade
Graham Brooks <graham@grahambrooks.com>
2015-10-14 22:09:07 -0400

    Added seed angular project to start work on the dashboard

--- web/app/src/main/java/kuona/web/Application.java
+++ web/app/src/main/java/kuona/web/Application.java
@@ -1,21 +1,33 @@
 package kuona.web;
 
 import com.google.gson.Gson;
-import kuona.web.controllers.ProjectMavenPomController;
-import org.elasticsearch.common.settings.ImmutableSettings;
-import org.elasticsearch.common.settings.Settings;
 import kuona.web.controllers.HomeController;
 import kuona.web.controllers.OrganisationsController;
+import kuona.web.controllers.ProjectMavenPomController;
+import org.apache.commons.cli.*;
+import org.elasticsearch.common.settings.ImmutableSettings;
+import org.elasticsearch.common.settings.Settings;
+
+import java.io.OutputStream;
+import java.io.PrintWriter;
 
 import static spark.Spark.get;
 import static spark.Spark.post;
+import static spark.SparkBase.port;
+import static spark.SparkBase.staticFileLocation;
 
 public class Application {
     public static void main(String[] args) {
+        final CommandLine commandLine = parseOptions(args);
+
+        port(Integer.parseInt(commandLine.getOptionValue('p', "9000")));
+
         Settings settings = ImmutableSettings.settingsBuilder().put("cluster.name", "elasticsearch_graham").build();
         Gson gson = new Gson();
 
-        get("/", new HomeController(settings), gson::toJson);
+        staticFileLocation("/public");
+
+        get("/settomgs", new HomeController(settings), gson::toJson);
 
         final OrganisationsController organisationsController = new OrganisationsController();
         get("/orgs", organisationsController::list, gson::toJson);
@@ -26,4 +38,35 @@ public class Application {
         get("/orgs/:org/projects/:project/metrics/java/maven/dependencies", mavenPomController::get, gson::toJson);
         post("/orgs/:org/projects/:project/metrics/java/maven/dependencies", mavenPomController::post, gson::toJson);
     }
+
+    protected static CommandLine parseOptions(String[] args) {
+        try {
+            Options options = commandLineOptions();
+
+
+            CommandLineParser parser = new DefaultParser();
+            return parser.parse(options, args);
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    protected static void printHelp(OutputStream output) {
+        PrintWriter writer = new PrintWriter(output);
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp(writer, 80, "java -jar kuona-maven-analyser.jar [options] <paths>", "\nOptions", commandLineOptions(), 3, 2, "\n See http://kuona.io");
+        writer.close();
+    }
+
+    protected static Options commandLineOptions() {
+        Options options = new Options();
+
+        options.addOption(new Option("p", "port", true, "Server port for HTTP traffic (default 9000)"));
+        options.addOption(new Option("h", "help", false, "Output this message"));
+        return options;
+    }
+
 }
