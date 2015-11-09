6ee641005361c784c094e8ba3719d51db36b6d5b
Graham Brooks <graham@grahambrooks.com>
2015-11-08 22:20:54 -0500

    Added printing help when a required cli parameter is missing

--- tools/snapci-analyser/src/main/java/kuona/snapci/analyser/Main.java
+++ tools/snapci-analyser/src/main/java/kuona/snapci/analyser/Main.java
@@ -4,46 +4,62 @@ import org.apache.commons.cli.*;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
+import java.io.OutputStream;
+import java.io.PrintWriter;
+
 import static kuona.snapci.analyser.utils.Utils.puts;
 
 public class Main {
 
     public static void main(String[] args) {
-
-        puts("Kuona snap-ci analyser");
-
-        CommandLine options = parseOptions(args);
-        KuonaAppConfig kuonaAppConfig = new KuonaAppConfig(
-                options.getOptionValue('u'),
-                options.getOptionValue('n')
-        );
-
-        MetricConfig config = new MetricConfig(kuonaAppConfig);
-
-        JobDataMap jobDataMap = new JobDataMap();
-        jobDataMap.put("kuonaAppConfig", kuonaAppConfig);
-        jobDataMap.put("metricConfig", config);
-
-        JobDetail job = JobBuilder.newJob(Collector.class)
-                .withIdentity("SnapCiCollector", "collectors")
-                .usingJobData(jobDataMap)
-                .build();
-
-        Trigger trigger = TriggerBuilder
-                .newTrigger()
-                .withIdentity("SnapCiTrigger", "collectors")
-                .withSchedule(
-                        SimpleScheduleBuilder.simpleSchedule()
-                                .withIntervalInSeconds(30).repeatForever())
-                .build();
         try {
-            Scheduler scheduler = new StdSchedulerFactory().getScheduler();
-            scheduler.start();
-            scheduler.scheduleJob(job, trigger);
+            puts("Kuona snap-ci analyser");
 
-        } catch (SchedulerException se) {
-            se.printStackTrace();
+            CommandLine options = parseOptions(args);
+            KuonaAppConfig kuonaAppConfig = new KuonaAppConfig(
+                    options.getOptionValue('u'),
+                    options.getOptionValue('n')
+            );
+
+            MetricConfig config = new MetricConfig(kuonaAppConfig);
+
+            JobDataMap jobDataMap = new JobDataMap();
+            jobDataMap.put("kuonaAppConfig", kuonaAppConfig);
+            jobDataMap.put("metricConfig", config);
+
+            JobDetail job = JobBuilder.newJob(Collector.class)
+                    .withIdentity("SnapCiCollector", "collectors")
+                    .usingJobData(jobDataMap)
+                    .build();
+
+            Trigger trigger = TriggerBuilder
+                    .newTrigger()
+                    .withIdentity("SnapCiTrigger", "collectors")
+                    .withSchedule(
+                            SimpleScheduleBuilder.simpleSchedule()
+                                    .withIntervalInSeconds(30).repeatForever())
+                    .build();
+            try {
+                Scheduler scheduler = new StdSchedulerFactory().getScheduler();
+                scheduler.start();
+                scheduler.scheduleJob(job, trigger);
+
+            } catch (SchedulerException se) {
+                se.printStackTrace();
+            }
+        } catch (RuntimeException re) {
+            System.err.println(re.toString());
+            printHelp(System.out);
+            return;
         }
+
+    }
+
+    protected static void printHelp(OutputStream output) {
+        PrintWriter writer = new PrintWriter(output);
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp(writer, 80, "java -jar snapci-analyser.jar [options] <paths>", "\nOptions", commandLineOptions(), 3, 2, "\n See http://kuona.io");
+        writer.close();
     }
 
     protected static CommandLine parseOptions(String[] args) {
